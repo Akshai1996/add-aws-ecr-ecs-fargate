10716a3034e0745cc31b2ef2befc5585df66feda
hrishi <hrishi@localhost>
2025-01-22 12:50:19 +0530

    update retention

--- infra/cloud_watch.tf
+++ infra/cloud_watch.tf
@@ -1,7 +1,7 @@
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudwatch_log_group
 resource "aws_cloudwatch_log_group" "logs" {
   name              = "/amazon-ecs/${var.name}/log"
-  retention_in_days = 10
+  retention_in_days = 14
   kms_key_id        = var.kms_key_id
  
 }
