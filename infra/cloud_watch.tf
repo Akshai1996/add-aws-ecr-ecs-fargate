3bb1f33b168d90f740b725ebf98bed8a2573f0bd
warlock_Gu <68325632+Akshai1996@users.noreply.github.com>
2025-01-22 12:31:25 +0530

    Update cloud_watch.tf

--- infra/cloud_watch.tf
+++ infra/cloud_watch.tf
@@ -1,7 +1,7 @@
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudwatch_log_group
 resource "aws_cloudwatch_log_group" "logs" {
   name              = "/amazon-ecs/${var.name}/log"
-  retention_in_days = 365
-  kms_key_id        = aws_kms_key.custom_kms_key.arn
-  depends_on        = [aws_kms_key.custom_kms_key]
+  retention_in_days = 10
+  kms_key_id        = "arn:aws:kms:us-east-1:008971661427:key/b1990d78-4db4-46cb-856b-d464bdc15152"
+ 
 }
