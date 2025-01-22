39b7462a254728cc72561c5cfda4f5c87323d5aa
hrishi <hrishi@localhost>
2025-01-22 12:40:23 +0530

    update bucket

--- deploy/backend.tf
+++ deploy/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "kunduso-terraform-remote-bucket"
+    bucket  = "nodeapp-terraform-remote-bucket"
     encrypt = true
     key     = "tf/add-aws-ecr-ecs-fargate/deploy-ecs.tfstate"
     region  = "us-east-1"
--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "kunduso-terraform-remote-bucket"
+    bucket  = "nodeapp-terraform-remote-bucket"
     encrypt = true
     key     = "tf/add-aws-ecr-ecs-fargate/terraform.tfstate"
     region  = "us-east-1"
