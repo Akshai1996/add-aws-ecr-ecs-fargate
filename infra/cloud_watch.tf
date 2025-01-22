1277cf6af85b8379055134651895252d224ab38d
warlock_Gu <68325632+Akshai1996@users.noreply.github.com>
2025-01-22 12:32:49 +0530

    Update cloud_watch.tf

--- infra/cloud_watch.tf
+++ infra/cloud_watch.tf
@@ -2,6 +2,6 @@
 resource "aws_cloudwatch_log_group" "logs" {
   name              = "/amazon-ecs/${var.name}/log"
   retention_in_days = 10
-  kms_key_id        = "arn:aws:kms:us-east-1:008971661427:key/b1990d78-4db4-46cb-856b-d464bdc15152"
+  kms_key_id        = var.kms_key_id
  
 }
