e11902b42def3258d9b45b27efe89b5967641027
hrishi <hrishi@localhost>
2025-01-23 12:38:30 +0530

    terraform backend updates

--- app/tf/backend.tf
+++ app/tf/backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
-    bucket  = "terraform-remote-state-076680484948"
+    bucket  = "nodeapp-terraform-remote-bucket"
     encrypt = true
     key     = "tf/add-aws-ecr-ecs-fargate/terraform.tfstate"
     region  = "us-east-1"
+    dynamodb_table = "terraform-backend-nodejs-app"
   }
 }
--- deploy/backend.tf
+++ deploy/backend.tf
@@ -4,5 +4,6 @@ terraform {
     encrypt = true
     key     = "tf/add-aws-ecr-ecs-fargate/deploy-ecs.tfstate"
     region  = "us-east-1"
+    dynamodb_table = "terraform-backend-nodejs-app"
   }
 }
